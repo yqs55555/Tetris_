4f7a11351e8da17a29a19ffcd580a58fd67e674e
BWT <wbt199794@163.com>
2017-07-24 11:58:48 +0800

    mprning_b

--- Tetris/Rank.cpp
+++ Tetris/Rank.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "Rank.h"
+#include"fstream"
 
 
 Rank::Rank()
@@ -18,22 +19,57 @@ Rank::~Rank()
 void Rank::textread()
 {
 	//将数据文件的数据写入三个数组中
+	string filenamePtn0 = "dataPtn0.txt";
+	readdata(filenamePtn0, scorePtn0);
+	string filenamePtn1 = "dataPtn1.txt";
+	readdata(filenamePtn1, scorePtn1);
+	string filenamePtn2 = "dataPtn2.txt";
+	readdata(filenamePtn2, scorePtn2);
+
+
+	
 }
 
 void Rank::textwrite(string str, int * a)
 {
 }
 
-void Rank::BubbleSort(int *& p)
+void Rank::BubbleSort(int *p)
 {
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10 - i - 1; j++)
+		{
+			if (p[j] < p[j + 1])
+			{
+				swap(p[j], p[j + 1]);
+			}
+		}
+	}
 }
+
 
 void Rank::swap(int & a, int & b)
 {
+	a = a + b;
+	b = a - b;
+	a = a - b;
 }
 
-void Rank::insert(int *& a, int n)
+void Rank::insert(int *a, int n)
 {
+	{
+		BubbleSort(a);
+		if (caninsert(a, n))
+		{
+			int position = caninsert(a, n) + 1;
+			for (int i = 10; i >position; i--)
+			{
+				a[i] = a[i - 1];
+			}
+			    a[position] = n;
+		}
+	}
 }
 
 int Rank::caninsert(int * array, int n)
@@ -41,7 +77,31 @@ int Rank::caninsert(int * array, int n)
 	return 0;
 }
 
+void Rank::readdata(string filename, int* score)
+{
+	fstream file;
+	file.open(filename, ios::in);
+	if (!file)
+		cout << "file not founded" << endl;
+	int pos = 0;
+	while (!file.eof())//是否到文件结尾
+	{
+		file >> score[pos];
+		pos++;
+		if (pos >= 100)
+			break;
+	}
+	file.close();
+}
+
 void Rank::display()
 {
-	//打印三个数组
+	CString res;
+	for (int i = 0; i < 10; i++)
+	{
+		CString test;
+		test.Format(_T("%d"), scorePtn0[i]);
+		res = res + test + '\n';
+	}
+	MessageBox(nullptr, res, L"分数", MB_OK);
 }
--- Tetris/Rank.h
+++ Tetris/Rank.h
@@ -11,12 +11,13 @@ public:
 private:
 	void textread();//读取数据文件
 	void textwrite(string str, int *a);//写入数据文件
-	void BubbleSort(int  *&p);//从大至小排列
+	void BubbleSort(int  *p);//从大至小排列
 	void swap(int &a, int &b);//交换位置
-	void insert(int *&a, int n);//插入分数
+	void insert(int *a, int n);//插入分数
 	int caninsert(int *array, int n);//判断是否插入
 	int scorePtn0[15];//经典模式
 	int scorePtn1[15];//残局模式
 	int scorePtn2[15];//随机变速模式
+	void readdata(string filename,int* score);
 };
 
